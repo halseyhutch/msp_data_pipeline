dcabe2be16ad93cd32d8c4ecf1870755c348f06e
halseyhutch <halseyhutch@gmail.com>
2022-11-08 18:48:12 -0500

    add a few more comments

--- schema.sql
+++ schema.sql
@@ -1,45 +1,50 @@
--- This table provides the overall information for a hospital, such as the state it is located in, name, address, and other descriptive features
+/* This table provides the overall information for a hospital,
+such as the state it is located in, name, address,
+and other descriptive features.
+
+Though zip codes uniquely identify states, and it is possible for
+state codes to change, it has only happened once in history,
+and it is not likely to occur again. */
 CREATE TABLE hospitals (
 	id text PRIMARY KEY,
-	state text,
 	name text,
 	address text,
 	city text,
+	state text,
 	zip integer,
 	fips_code text,
-	lat integer,
-	long integer,
+	lat numeric,
+	long numeric,
 	hospital_type text,
 	ems_provided boolean
 );
 
 
--- This table summarizes the number of available beds for each hospital by date
+/* This table summarizes the number of available beds
+for each hospital by date. */
 CREATE TABLE hospital_beds (
 	hospital_id text REFERENCES hospitals (id),
-	record_date date,
-	adult_beds_available integer,
-	pediatric_beds_available integer,
-	adult_beds_used integer,
-	pediatric_beds_used integer,
-	icu_beds_used integer,
-	inpatients_beds_used_covid integer,
-	staffed_adult_icu_patients_confirmed integer,
+	record_date date CHECK (record_date <= CURRENT_DATE),
+	adult_beds_available integer CHECK (adult_beds_available >= 0),
+	pediatric_beds_available integer CHECK (pediatric_beds_available >= 0),
+	adult_beds_used integer CHECK (adult_beds_used >= 0),
+	pediatric_beds_used integer CHECK (pediatric_beds_used >= 0),
+	icu_beds_used integer (icu_beds_used >= 0),
+	inpatients_beds_used_covid integer (inpatients_beds_used_covid >= 0),
+	staffed_adult_icu_patients_confirmed integer (staffed_adult_icu_patients_confirmed >= 0),
 	PRIMARY KEY (hospital_id, record_date)
 );
 
 
--- This table provides an overall quality rating for a hospital which can be tracked at various dates
+/* This table provides an overall quality rating
+for a hospital which can be tracked at various dates.
+
+Note that we put this data in a separate table from hospital_beds
+since it is highly unlikely to be contemporaneous. A single temporal table
+would have many more nulls than this alternative. */
 CREATE TABLE hospital_quality (
 	hospital_id text REFERENCES hospitals (id),
-	record_date date,
+	record_date date CHECK (record_date <= CURRENT_DATE),
 	quality_rating integer,
 	PRIMARY KEY (hospital_id, record_date)
 );
-
-
-/*Entities: hospitals, hostipal_bed, hospital_quality*/
-
-/*we choose them by checking how each variables are related to each other and see whether they can be connect by the same variable. */
-
-/*In order to avoid redundency, we design our databse with the help of heritage to insure that there are no repeated information stored.*/
